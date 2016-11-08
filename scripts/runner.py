7fb393c1e0a8cf825356aaf988bb8b09d4797d9d
Christopher Deutsch <chrisdeutsch0@googlemail.com>
2016-11-08 18:07:47 +0100

    All the bugfixes

--- scripts/runner.py
+++ scripts/runner.py
@@ -1,11 +1,12 @@
 import argparse
 import json
+import gc
 
 def get_identifier():
     parser = argparse.ArgumentParser()
     parser.add_argument("arrayid", type=int, help="ArrayID")
     args = parser.parse_args()
-    return "{:04}".format()
+    return "{:04}".format(args.arrayid)
 
 if __name__ == "__main__":
     identifier = get_identifier()
@@ -18,23 +19,27 @@ if __name__ == "__main__":
     import xgboost as xgb
     
     # Load data
-    invars = config.variables + [config.weight, config.classlabel]
-    data_train = pd.read_hdf(config.train, columns=invars)
-    dtrain = xgb.DMatrix(data_train[config.variables],
-                         label=data_train[config.classlabel],
-                         weight=data_train[config.weight])
+    invars = config["variables"] + [config["weight"], config["classlabel"]]
+    data_train = pd.read_hdf(config["train"], columns=invars)
+    dtrain = xgb.DMatrix(data_train[config["variables"]],
+                         label=data_train[config["classlabel"]],
+                         weight=data_train[config["weight"]])    
+    # Free memory
     del data_train
+    gc.collect()
 
-    data_test = pd.read_hdf(config.test, columns=invars)
-    dtest = xgb.DMatrix(data_test[config.variables],
-                        label=data_test[config.classlabel],
-                        weight=data_test[config.weight])
+    data_test = pd.read_hdf(config["test"], columns=invars)
+    dtest = xgb.DMatrix(data_test[config["variables"]],
+                        label=data_test[config["classlabel"]],
+                        weight=data_test[config["weight"]])
+    # Free memory
     del data_test
+    gc.collect()
 
     evallist = [(dtrain, "train"), (dtest, "test")]
 
     # TODO: move this to json?
-    params = config.config.copy()
+    params = config["config"].copy()
     params["eval_metric"] = ["auc", "rmse"]
     params["nthread"] = 8
     num_rounds = 20
@@ -42,9 +47,9 @@ if __name__ == "__main__":
 
     bst = xgb.train(params, dtrain, num_rounds, evallist,
                     early_stopping_rounds=early_stopping)
-    bst.save_model("{}.model".format(config.identifier))
+    bst.save_model("{}.model".format(config["identifier"]))
     # End of classifier specific code -----------------------------------------
     
-    config.processed = True
-    with open(args.file, "w") as f:
-        f.dump(config)
+    config["processed"] = True
+    with open(identifier + ".json", "w") as f:
+        json.dump(config, f, indent=4)
