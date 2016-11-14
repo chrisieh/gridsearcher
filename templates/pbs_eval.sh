2b59cf70dea98c455b3b464174b99299ab45bf85
Christopher Deutsch <chrisdeutsch0@googlemail.com>
2016-11-14 20:44:40 +0100

    Added evaluation pbs script

--- templates/pbs_eval.sh
+++ templates/pbs_eval.sh
@@ -1,9 +1,9 @@
-#PBS -N GridSearcher-Train
+#PBS -N GridSearcher-Eval
 #PBS -l nodes=1:ppn=4
 #PBS -l mem=4g
 #PBS -l file=10g
 ## Queues: vshort (10min), short (1h 30min), medium (24h), long (1 week)
-#PBS -q medium
+#PBS -q short
 #PBS -j oe
 
 source /etc/profile
@@ -14,6 +14,6 @@ cd $PBS_O_WORKDIR
 setupATLAS
 lsetup root
 source $HOME/env/tid/bin/activate
-python scripts/runner.py $PBS_ARRAYID
+python gridsearcher2/scripts/xgb_eval.py $PBS_ARRAYID
 
 echo "Done"
